16136885d72594ee3f17294b2191988db48a9f5d
Bryan <bmiller@tb.yearup.org>
2022-12-05 13:00:36 -0500

    Started javascript for registering user

---
+++ registration/register.js
@@ -0,0 +1,38 @@
+"use strict";
+
+regForm.onsubmit = function (event) {
+    // Prevent the form from refreshing the page,
+    // as it will do by default when the Submit event is triggered:
+    event.preventDefault();
+
+    // We can use loginForm.username (for example) to access
+    // the input element in the form which has the ID of "username".
+    const regData = {
+        username: registerForm.username.value,
+        fullname: registerForm.fullname.value,
+        password: registerForm.password.value,
+    }
+
+    // Disables the button after the form has been submitted already:
+    registerForm.loginButton.disabled = true;
+
+    // Time to actually process the login using the function from auth.js!
+    register(regData);
+};
+
+function register (regData) {
+    const options = { 
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(regData),
+    };
+
+    return fetch(api + "/auth/login", options)
+        .then(response => response.json())
+        .then(regData => {
+            window.localStorage.setItem("login-data", JSON.stringify(regData));
+            window.location.assign("/index.html");  // redirect
+        });
+}
